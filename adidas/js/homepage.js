ef083ed429182f665ad3dbbefea41789949e0b54
zgdduangzzz <50448100+zgdduangzzz@users.noreply.github.com>
2019-05-09 19:37:48 +0800

    Add files via upload

---
+++ adidas/js/homepage.js
@@ -0,0 +1,108 @@
+$(function(){
+	$.ajax({
+		type:"get",					//请求方法
+		url:"js/homepage.json",			//接口
+		async:true,					//是否异步
+		success:function (data) {	//请求成功执行
+//			console.log(data.Shoetype[0])
+//			console.log(data.shangpin[0])
+			$.each(data.Shoetype1,function (i,t) {
+				$("<li class='Extention'><a href='#'>"+t+"</a></li>").appendTo('.nav-pills1')
+				$('.Extention').css({
+					"display":"inline-block",
+					"text-decoration":"none",
+					"width":"66px",
+					"height":"50px",
+					"padding-left":"15px",
+					"padding-right":"15px",
+					"line-height":"50px",
+					 "float":"left"
+				})
+				$('.Extention a').css({
+					"display":"inline-block",
+					"width":"36px",
+					"height":"50px",
+					"color":"white",
+					"font-size":"18px",
+					"font-weight":"bold"
+				})
+			})
+			$.each(data.Shoetype2,function (i,t) {
+				$("<li class='Extention'><a href='#'>"+t+"</a></li>").appendTo('.nav-pills2')
+				$('.Extention').css({
+					"display":"inline-block",
+					"text-decoration":"none",
+					"width":"66px",
+					"height":"50px",
+					"padding-left":"15px",
+					"padding-right":"15px",
+					"line-height":"50px",
+					 "float":"left"
+				})
+				$('.Extention a').css({
+					"display":"inline-block",
+					"width":"36px",
+					"height":"50px",
+					"color":"white",
+					"font-size":"18px",
+					"font-weight":"bold"
+				})
+				$('.Extention:eq(5)').css({
+					"display":"inline-block",
+					"width":"72px",
+					"height":"50px",
+					"color":"white",
+					"font-size":"18px",
+					"font-weight":"bold",
+					"padding-left":"15px",
+					"padding-right":"15px"
+				})
+				$('.Extention:eq(5) a').css({
+					"display":"inline-block",
+					"width":"72px",
+					"height":"50px",
+					"color":"white",
+					"font-size":"18px",
+					"font-weight":"bold",
+				})
+			})
+			$.each(data.Carefulrecommendationtop,function (i,t) {
+				$("<div class='drop-down-menu-body-left-content'><a href='#'>"+t+"</a></div>").appendTo('.drop-down-menu-body-left ul:eq(0)')
+				$('.drop-down-menu-body-left-content').css({
+					"text-decoration":"none",
+					"text-align":"left",
+					"margin-top":"2%"
+				})
+				$('.drop-down-menu-body-left-content a').css({
+					"font-size":"15px",
+					"text-align":"left"
+				})
+				/*$('.Extention:eq(5)').css({
+					"display":"inline-block",
+					"width":"72px",
+					"height":"50px",
+					"color":"white",
+					"font-size":"18px",
+					"font-weight":"bold",
+					"padding-left":"15px",
+					"padding-right":"15px"
+				})
+				$('.Extention:eq(5) a').css({
+					"display":"inline-block",
+					"width":"72px",
+					"height":"50px",
+					"color":"white",
+					"font-size":"18px",
+					"font-weight":"bold",
+				})*/
+			})
+			console.log($('.Extention:eq(2)'))
+		},
+		error:function (sd) {			//请求失败执行
+			console.log(sd)
+		}
+	});
+	$('.nav-pills1').on('mouseover',function () {
+		var Newdivnav = "<div class=''></div>";
+	})
+})
